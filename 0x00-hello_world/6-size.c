c62204d4073d9d5b48116d4912e3321ccd1c8bad
samochieng <sam30philosophy@gmail.com>
2022-09-11 22:12:56 +0000

    prints the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-printf("Size of a char:%lu\n byte(s)", sizeof(char));
+printf("Size of a char:%lu\n", sizeof(char) byte(s));
 printf("Size of an interger:%lu\n", sizeof(int));
 printf("Size of a long interger:%lu\n", sizeof(long int));
 printf("Size of a long,long interger:%lu\n", sizeof(long long int));
