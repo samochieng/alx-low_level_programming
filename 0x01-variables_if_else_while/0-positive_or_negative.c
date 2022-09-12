fdae2bb6ec3b974ed7013b4c3dd9bf550f552fb0
samochieng <sam30philosophy@gmail.com>
2022-09-12 12:53:06 +0000

    assign a random number to the variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*more headers goes there 
- *Description prints is a n is positive or negative
+/**
+ *main-entry point print string values from if statement
+ *Description main prints if a n is positive or negative
  *Return: 0
  */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 int n;
 
 	srand(time(0));
 		n = rand() - RAND_MAX / 2;
-			if (n > 0){
+			if (n > 0)
+			{
 				printf("%d is positive\n", n);
 				}
-			else if (n < 0){
+			else if (n < 0)
+			{
 				printf("%d is negative\n", n);
 				}
-			else if (n == 0){
+			else if (n == 0)
+			{
 				printf("%d is zero\n", n);
 				}
 			return (0);
