06cebbd6a4ca240040527d92af0b612087bda52a
samochieng <sam30philosophy@gmail.com>
2022-09-12 21:41:05 +0000

    prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*mian - prints output using printf
+*
+* Description: prints all possible combinations of single-digit numbers
+* Return: 0;
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
+
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -23,5 +23,9 @@ prints all single digit numbers of base 10 starting from 0
 prints the lowercase alphabet in reverse
 
 8-print_base16.c
-prints all the numbers of base 16 in lowercase,
+prints all the numbers of base 16 in lowercase
+
+9-print_comb.c
+prints all possible combinations of single-digit numbers
+
 
