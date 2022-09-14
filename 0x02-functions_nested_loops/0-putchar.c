3631ab6f28abd7c35e554c2b2e02c8fb5a190dce
samochieng <sam30philosophy@gmail.com>
2022-09-14 16:53:11 +0000

    program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+/**
+*
+* main - prints putchar to stdout
+*
+* Description: program that prints _putchar, followed by a new line
+*
+* Return: 0;
+*/
+int main()
+{
+	_putchar.c();
+	putchar("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+0-putchar.c
+program that prints _putchar, followed by a new line
