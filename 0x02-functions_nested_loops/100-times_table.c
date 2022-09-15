978aafad89419ee32a196ff6569f0af5af99771f
samochieng <sam30philosophy@gmail.com>
2022-09-15 19:37:20 +0000

    fix 2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,40 +8,42 @@ void print_times_table(int n)
 {
 	int digit, mult, result;
 
-	if (n <= 15 && n >= 0)
+	if (n <= 14 && n >= 0)
 	{
 		for (digit = 0; digit <= n; digit++)
 		{
-		_putchar('0');
+			for (mult = 0; mult <= n; mult++)
+			{
+				result = digit * mult
+				if (result > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else if (result > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else
+				{
+					if (mult != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(result + '0');
+				}
+			}
+			_putchar('\n');
 		}
-		for (mult = 1; mult <= n; mult++)
-		{
-		_putchar(',');
-		_putchar(' ');
-
-		result = digit * mult;
-
-		if (result <= 99)
-		_putchar(' ');
-
-		if (result <= 9)
-		_putchar(' ');
-
-		if (result >= 100)
-		{
-		_putchar((result / 100) + '0');
-		_putchar((result / 10) % 10 + '0');
-		}
-		else if (result <= 99 && result >= 10)
-		{
-		_putchar((result / 10) + '0');
-		}
-		_putchar((result % 10) + '0');
-		}
-		_putchar('\n');
-		}
+	}
 }
-
-
-
-
