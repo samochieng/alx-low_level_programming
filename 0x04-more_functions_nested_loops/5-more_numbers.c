26f4e432909fdd25a305c9284151e6cba200214a
samochieng <sam30philosophy@gmail.com>
2022-09-15 23:32:18 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*more_numbers - prints 10x the numbers from 0 to 14
+*
+*Return: 0;
+*/
+void more_numbers(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		for (nz = 0; n2 <= 14; n2++)
+		{
+			if (n2 > 9)
+			{
+				_putchar((n2 / 10) + '0');
+			}
+			_putchar((n2 % 10) + '0');
+		}
+		_putchar(10);
+	}
+}
