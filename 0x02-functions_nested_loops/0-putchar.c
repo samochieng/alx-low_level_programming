d17a9f24b1050a929ad1349c633df2d9eb5706fd
samochieng <sam30philosophy@gmail.com>
2022-09-15 13:35:09 +0000

    fix 2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * * main - prints putchar to stdout
- * *
- * * Description: program that prints variable, followed by a new line
- * *
- * * Return: 0;
- * */
+ * main - prints putchar to stdout
+ *
+ * Description: program that prints variable, followed by a new line
+ *
+ * Return: 0;
+ */
 int main(void)
 {
 	_putchar('_');
