06ae64ea645b309c2ae3d352a006b6cf4d49fa45
samochieng <sam30philosophy@gmail.com>
2022-09-15 23:09:51 +0000

    more functions

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,9 +8,9 @@ void print_numbers(void)
 {
 	int k;
 
-	for (k = 0; k <= 9; k++)
+	for (k = 48; k < 58; k++)
 	{
 		_putchar(k + '0');
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,6 +6,8 @@
 */
 void print_most_numbers(void)
 {
+	int n;
+
 	for (n = 48; n < 58; n++)
 	{
 		if ((n != 50) || (n != 52))
