9f55a9319654bbe94425311c108be61770c3b5a1
samochieng <sam30philosophy@gmail.com>
2022-09-15 23:37:24 +0000

    more functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,9 @@ void more_numbers(void)
 
 	for (n1 = 0; n1 < 10; n1++)
 	{
-		for (nz = 0; n2 <= 14; n2++)
+		for (n2 = 0; n2 <= 14; n2++)
 		{
-			if (n2 > 9)
+			if (n2 >= 10)
 			{
 				_putchar((n2 / 10) + '0');
 			}
