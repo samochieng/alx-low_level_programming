b7ee09ff329734d9ec7d82450ee80725d3eaf273
samochieng <sam30philosophy@gmail.com>
2022-09-19 18:24:27 +0000

    return lenght of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
 *_strlen -> function returns the length of a string
@@ -9,10 +10,9 @@ int _strlen(char *s)
 {
 	int len;
 
-	while (*s != '\0')
+	for (; *s != '\0'; s++)
 	{
 		len = len + 1;
-		*s = *s + 1;
 	}
 	return (len);
 }
