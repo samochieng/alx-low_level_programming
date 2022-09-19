091b6300a748fab4cbde9a74444b7dfb162ba77a
samochieng <sam30philosophy@gmail.com>
2022-09-19 19:13:09 +0000

    prints a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_rev -> function that prints string in reverse
+*@s: parameter
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
