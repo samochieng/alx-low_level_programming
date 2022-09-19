27da837452db7c2b24e1e8abf03016fa7b29a36b
samochieng <sam30philosophy@gmail.com>
2022-09-19 21:05:21 +0000

    array of n numbers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+#include "main.h"
+
+/**
+* print_array -> prints n elements of an array of intergers
+* @a: parameter
+* @n: parameter
+*/
+void print_array(int *a, int n)
+{
+	int p;
+
+	for (p = 0; p < n; p++)
+	{
+		if (p != n - 1)
+		{
+			printf("%d, ", a[p]);
+		}
+		else
+		{
+			printf("%d", a[p]);
+		}
+		_putchar('\n');
+	}
+}
+
