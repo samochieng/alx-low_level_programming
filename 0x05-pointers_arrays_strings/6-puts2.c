97be99bdb68c62ee5d502cbe1547c3f4990b8d59
samochieng <sam30philosophy@gmail.com>
2022-09-19 20:29:46 +0000

    prints every character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,15 +6,13 @@
 */
 void puts2(char *str)
 {
-	int i = 0;
+	int i, len;
 
-	while (*(str + 1) != '\0')
+	len = strlen(str)
+
+	for (i = 0; i < len; i++)
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(*(str + 1));
-		}
-		i++;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
