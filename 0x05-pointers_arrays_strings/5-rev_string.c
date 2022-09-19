0a95a3d84384cf389c128685e317388e29cdde6c
samochieng <sam30philosophy@gmail.com>
2022-09-19 19:57:42 +0000

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*rev_string -> function to reverse a string
+*@s: parameter
+*/
+void rev_string(char *s)
+{
+	char tmp;
+	int i, length1, length2;
+
+	length1 = 0;
+	length2 = 0;
+
+	while (s[length1] != '\0')
+	{
+		length1++;
+	}
+		length2 = length1 - 1;
+	for (i = 0; i < length1; 1++)
+	{
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 -= 1;
+	}
+}
+
+
