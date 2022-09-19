64a03b738be9c99ee328d95eca58f334548d8209
samochieng <sam30philosophy@gmail.com>
2022-09-19 20:06:05 +0000

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,9 +13,12 @@ void rev_string(char *s)
 	length2 = 0;
 
 	while (s[length1] != '\0')
+	{
 		length1++;
+	}
 
-		length2 = length1 - 1;
+	length2 = length1 - 1;
+
 	for (i = 0; i < length1 / 2; i++)
 	{
 		tmp = s[i];
