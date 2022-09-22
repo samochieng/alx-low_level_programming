e91a65b6895cbc3e1c922889d39bf97b3532f625
samochieng <sam30philosophy@gmail.com>
2022-09-22 15:31:54 +0000

    hot fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,15 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int p = 0;
+	int i = 0, op = 0;
 
-	while (p == 0)
+	while (op == 0)
 	{
-		if (*(s1 + i) == '\0') &&(*(s2 + i) == '\0')
+		if ((*(s1 + i) == '\0') &&(*(s2 + i) == '\0'))
 
 			break;
 
-		p = *(s1 + i) - *(s2 + i);
+		op = *(s1 + i) - *(s2 + i);
 
 		i++;
 	}
