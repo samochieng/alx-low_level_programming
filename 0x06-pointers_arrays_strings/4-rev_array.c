653f80cb904de973487c630d970f2ae0e7eec330
samochieng <sam30philosophy@gmail.com>
2022-09-22 15:54:14 +0000

    hot fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,16 +7,13 @@
 */
 void reverse_array(int *a, int n)
 {
-	int i, j, temp;
+	int index, temp;
 
-	for (i = 0; i < n - 1; i++)
+	for (index = n - 1; index >= n / 2; index--)
 	{
-		for (j = i + 1; j > 0; j--)
-		{
-			temp = *(a + j);
-			*(a + j) = *(a + (j - 1);
-			*(a + (j - 1) = temp;
-		}
+		temp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = temp;
 	}
 }
 
