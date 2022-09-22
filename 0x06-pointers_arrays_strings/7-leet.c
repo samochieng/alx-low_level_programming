3700b8b674b91350a68d5b2c9820343858869971
samochieng <sam30philosophy@gmail.com>
2022-09-22 17:17:35 +0000

    1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,13 +12,14 @@ char *leet(char *str)
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'}
 
 	while (str[indx1])
+	{
 
 		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
 			if ((str[indx1]) == leet[indx2]) || (str[indx1] - 32 == leet[indx2])
 				str[indx1] = indx2 + '0';
 		}
-		indx1++
+		indx1++;
 	}
 	return (str);
 }
