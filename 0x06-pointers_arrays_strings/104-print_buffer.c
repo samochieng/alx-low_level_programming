c611546b680c098a8458e2dd4a92018929a7e19a
samochieng <sam30philosophy@gmail.com>
2022-09-22 21:16:07 +0000

    print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include "unistd.h"
 
 /**
 * print_buffer -> prints a buffer 10 bytes per line
@@ -9,50 +8,38 @@
 */
 void print_buffer(char *b, int size)
 {
-	int j, k;
+	int j, k, l;
 
-	for (j = 0; j <= 9; j++)
+	if (size <= 0)
+		printf("\n");
+	else
 	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
-			printf("  ");
-		if (j % 2)
-			putchar(' ');
-	}
-	for (k = 0; k <= s; k++)
-	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
+		}
 	}
 }
 
-/**
-* print_buffer - prints a buffer
-* @b: buffer to print
-* @size: size of buffer
-*
-* Return: void
-*/
-void print_buffer(char *b, int size)
-{
-	int i;
 
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
-	{
-		printf("%08x: ", i * 10);
-		if (i < size / 10)
-		{
-			print_line(b, 9, i);
-		}
-		else
-		{
-			print_line(b, size % 10 - 1, i);
-		}
-		putchar('\n');
-	}
-	if (size == 0)
-		putchar('\n');
-}
+
