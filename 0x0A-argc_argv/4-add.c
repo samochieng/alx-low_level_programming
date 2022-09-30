fcb2df31ffe3f71659b5f339197f52a3547c304c
samochieng <sam30philosophy@gmail.com>
2022-09-30 21:28:23 +0000

    adds two positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,5 @@
 #include "stdio.h"
 #include "stdlib.h"
-#include "string.h"
-#include "ctype.h"
 
 /**
 * main -> adds two positve numbers
@@ -12,30 +10,19 @@
 */
 int main(int argc, int *argv[])
 {
-	int sum = 0, i;
+	int num, dig, sum = 0;
 
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
+		for (num = 1; num < argc; num++)
 		{
-			int b;
-			char *str;
-
-			str = argv[i];
-
-			for (b = 0; str[b] != '\0'; b++)
+			for (dig = 0; argv[num][dig]; dig++)
 			{
-				if (str[b] < 48 || str[b] > 57)
+				if (argv[num][dig] < '0' || argv[num][dig] > '9')
 				{
 					printf("Error\n");
 					return (1);
 				}
 			}
-		}
-	}
-	for (i = 1; i < argc; i++)
-	{
-		sum += atoi(argv[i]);
+			sum += atoi(argv[num]);
 	}
 	printf("%d\n", sum);
 	return (0);
