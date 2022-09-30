c13df548349fda7fd4a27664f1f2cbb1fd0467c7
samochieng <sam30philosophy@gmail.com>
2022-09-30 15:20:50 +0000

    prints filename

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+* main -> prints the name of the program
+* @argc: argument count
+* @argv: argument vector
+* Return: 0;
+*/
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argc[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+0x0A. C - argc, argv
+There are at least two arguments to main: argc and argv. The first of these is a count of the arguments supplied to the program and the second is an array of pointers to the strings which are those arguments
