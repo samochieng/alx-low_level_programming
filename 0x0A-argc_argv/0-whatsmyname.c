da176390900ad19f3dfcb2e0e03194df64ff3f1f
samochieng <sam30philosophy@gmail.com>
2022-09-30 15:44:44 +0000

    prints filename

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "stdio.h"
+#include <stdio.h>
 
 /**
 * main -> prints the name of the program
@@ -9,7 +9,7 @@
 */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argc[0]);
+	printf("%s\n", argv[0]);
 
 	(void)argc;
 
