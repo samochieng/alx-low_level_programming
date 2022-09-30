a7600cf4f83e6fdcc80362d58d3567c1e3ced2f2
samochieng <sam30philosophy@gmail.com>
2022-09-30 19:35:33 +0000

    prints all arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,22 @@
 #include "main.h"
-#include "stdio.h"
+#include <stdio.h>
 
 /**
  * main -> prints all the arguments it receives
- * @args: argument counter
+ * @argc: argument counter
  * @argv: argument vector
  * Return: 0;
  */
-int main(int args, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%d\n", args - 1);
+	int count = 0;
 
-	void(argv);
-	
+	if (argc > 0) 
+	{
+		while (count < argc)
+		{
+			printf("%s\n", argv[count]);
+		}
+	}
 	return (0);
 }
