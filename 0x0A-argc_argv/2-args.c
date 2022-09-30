91fa2e9af19f98d2a693b6c0d7d6e85d9b5b330b
samochieng <sam30philosophy@gmail.com>
2022-09-30 15:25:34 +0000

    prints all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+* main -> prints all the arguments it receives
+* @argc: argument counter
+* @argv: argument vector
+* Return: 0;
+*/
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
+
