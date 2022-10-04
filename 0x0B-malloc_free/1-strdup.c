0b9757a4aa18d4b2aea6923b291c2b92846f1d99
samochieng <sam30philosophy@gmail.com>
2022-10-04 18:23:53 +0000

    pointer to string copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+#include "stdlib.h"
+
+/**
+* _strdup -> returns a pointer to a newly allocated space in memory
+* @str: pointer to memory
+*
+* Return: eturns a pointer to the duplicated string or NULL
+*/
+char *_strdup(char *str);
+{
+	int i, j = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	s = (char *)malloc(i * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+	while (j < i)
+	{
+		s[j] = str[j];
+		j++;
+	}
+	s[j] = '\0';
+	return (s);
+}
+
