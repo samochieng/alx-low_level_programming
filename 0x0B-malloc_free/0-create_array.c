8971e80d491ced274ac5c75cb0830cef53abad5c
samochieng <sam30philosophy@gmail.com>
2022-10-04 17:21:14 +0000

    initialize array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* create_array -> function that creates an array of chars
+* @size: parameter for size of the array
+* @c: parameter for specific char assigned for initialization of array
+*
+* Return: pointer to the array, or NULL if it fails
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+	s = (char *)malloc(size 8 sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] - c;
+
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+0x0B. C - malloc, free
+learn to dynamically allocate memory in your C program using standard library functions: malloc() and free()
