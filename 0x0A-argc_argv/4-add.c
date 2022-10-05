ca3336d0d960580efa28c910a70a3f6b110ce8e5
samochieng <sam30philosophy@gmail.com>
2022-10-05 20:31:16 +0000

    hotfix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,30 +11,23 @@
 */
 int main(int argc, int *argv[])
 {
-	int sum = 0, i;
-
-	if (argc > 1)
+	int a = 0, i, j;
+	
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 0; i < argc; i++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			int b;
-			char *str;
-			
-			str = argv[i];
-			for (b = 0; str[b] != '\0'; b++)
+			if (isdigit(argv[i][j]) == 0)
 			{
-				if (str[b] < 48 || str[b] > 57)
-				{
-					printf("Error\n");
-					return (1);
-				}
+				puts("Error");
+				return (1);
 			}
 		}
 	}
 	for (i = 1; i < argc; i++)
 	{
-		sum += atoi(argv[i]);
+		a += atoi(argv[i]);
 	}
-	printf("%d\n", sum);
+	printf("%d\n", a);
 	return (0);
 }
