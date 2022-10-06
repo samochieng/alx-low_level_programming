2632fd81eb3ab21545510c15bb542bc3fd647bb7
samochieng <sam30philosophy@gmail.com>
2022-10-06 22:25:05 +0000

    hotfix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,11 @@ int _putchar(char c);
 #include <stdio.h>
 #include <limits.h>
 #include <stdlib.h>
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
 
 #endif /*MAIN_H*/
