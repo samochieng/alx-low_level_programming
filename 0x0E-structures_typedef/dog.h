1cc7ef5d68cc643b803755dce833590ff2db1d8e
samochieng <sam30philosophy@gmail.com>
2022-10-11 15:56:06 +0000

    new type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,11 +17,11 @@ struct dog
 
 };
 
-typedef struct dog dog_v;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_v new_dog(char *name, float age, char *owner);
-void free_dog(dog_v *d);
+dog_t new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 
 
