2dd22537820a6f0144d75396a9121ba7336a1b37
samochieng <sam30philosophy@gmail.com>
2022-10-11 23:31:31 +0000

    preprocessors

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef 3_FUNCTION_LIKE_MACRO_H
 #define 3_FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x < 0) ? (x) * (-1) : (x))
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
 
 #endif /*3_FUNCTION_LIKE_MACRO_H*/
