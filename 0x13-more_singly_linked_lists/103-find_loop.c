ff002ab6ed5683d95aba59f05fffedc431085464
samochieng <sam30philosophy@gmail.com>
2022-10-28 21:46:12 +0000

    hotfix

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,35 +8,28 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *p2;
-	listint_t *prev;
+	listint_t *tortoise, *hare;
 
-	p2 = head;
-	prev = head;
-	while (head && p2 && p2->next)
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
 	{
-		head = head->next;
-		p2 = p2->next->next;
-
-		if (head == p2)
+		if (tortoise == hare)
 		{
-			head = prev;
-			prev =  p2;
-			while (1)
+			tortoise = head;
+			while (tortoise != hare)
 			{
-				p2 = prev;
-				while (p2->next != head && p2->next != prev)
-				{
-					p2 = p2->next;
-				}
-				if (p2->next == head)
-					break;
-
-				head = head->next;
+				tortoise = tortoise->next;
+				hare = hare->next;
 			}
-			return (p2->next);
+			return (tortoise);
 		}
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
 	}
 	return (NULL);
 }
-
